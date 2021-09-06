70a44d8791b662569ee52f1fbe43a25437bce9a5
taylor <taylormoon@foxmail.com>
2021-09-06 12:45:30 +0800

    add bin

---
+++ bin/publish.sh
@@ -0,0 +1,14 @@
+cd /Users/taylor/codes/open_source/taylor0931.github.com
+echo '准备编译了'
+
+hugo 
+
+echo '准备提交代码了'
+
+git add .
+
+git commit -m "upadte"
+
+git push
+
+echo '代码提交完成，请用【Command + Shift + R】 强制刷新浏览器来查看效果。'
